4f4deba924d233ffe3dad1600997531048242730
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-07 12:09:52 -0400

    feat: add SystemUI material host contracts

---
+++ src/main/java/com/hellovoid/liquidui/glass/systemui/SystemUiGlassDomain.java
@@ -0,0 +1,12 @@
+package com.hellovoid.liquidui.glass.systemui;
+
+/** Stable feature-domain identity for shared SystemUI glass nodes. */
+public enum SystemUiGlassDomain {
+    NOTIFICATION,
+    CONTROL_CENTER,
+    VOLUME,
+    MEDIA,
+    STATUS_BAR,
+    KEYGUARD,
+    MISC
+}
