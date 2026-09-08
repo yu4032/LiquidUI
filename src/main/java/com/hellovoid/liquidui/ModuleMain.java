ee8f16e9a1ea91751141927daa2f4dc85310ad2b
yu4032 <4032542465@smail.lnu.edu.cn>
2026-09-08 02:28:33 -0400

    feat: register ongoing activity chip glass hook

--- src/main/java/com/hellovoid/liquidui/ModuleMain.java
+++ src/main/java/com/hellovoid/liquidui/ModuleMain.java
@@ -20,6 +20,7 @@ import com.hellovoid.liquidui.glass.plugin.MiuiControlCenterPluginGlassSession;
 import com.hellovoid.liquidui.glass.plugin.MiuiSecondaryPanelPluginGlassSession;
 import com.hellovoid.liquidui.glass.plugin.MiuiSystemUiPluginGlassHook;
 import com.hellovoid.liquidui.glass.plugin.MiuiVolumePluginGlassSession;
+import com.hellovoid.liquidui.glass.statusbar.StatusBarGlassHook;
 import com.hellovoid.liquidui.hook.HookRegistryReport;
 import com.hellovoid.liquidui.hook.SystemUiHookRegistry;
 import com.hellovoid.liquidui.target.FrameworkPackageVersionReader;
@@ -111,6 +112,9 @@ public final class ModuleMain extends XposedModule {
                             new Api101AfterMethodHookBackend(config.diagnosticsEnabled()),
                             processGlassCore),
                     new MediaOutputDialogGlassHook(
+                            new Api101AfterMethodHookBackend(config.diagnosticsEnabled()),
+                            processGlassCore),
+                    new StatusBarGlassHook(
                             new Api101AfterMethodHookBackend(config.diagnosticsEnabled()),
                             processGlassCore),
                     new MiuiSystemUiPluginGlassHook(
